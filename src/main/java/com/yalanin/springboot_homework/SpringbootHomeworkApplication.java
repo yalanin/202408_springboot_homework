e48e4a8ce5c7d075a2169b6391724523eae0130c
yalanin <yalanin@gmail.com>
2024-08-29 08:13:22 +0800

    專案依賴功能設定

---
+++ src/main/java/com/yalanin/springboot_homework/SpringbootHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.yalanin.springboot_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yalanin/springboot_homework/SpringbootHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yalanin.springboot_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
